a332dbe94cc1da6d2febf9c6485df710790f3593
VickusWan <vuong@mail.sfsu.edu>
2023-10-23 10:37:23 -0700

    static portion

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
+const path = require("path");
 const express = require("express");
+const createError = require("http-errors");
+const requestTime = require("./middleware/request-time");
+
+
 const app = express();
+
+app.use(requestTime);
+
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "static")));
+
 const rootRoutes = require("./routes/root");
+
 app.use("/", rootRoutes);
+
+/** Existing server.js content **/
+app.use((request, response, next) => {
+next(createError(404));
+});
+
+
 app.listen(PORT, () => {
 console.log(`Server started on port ${PORT}`);
 });
