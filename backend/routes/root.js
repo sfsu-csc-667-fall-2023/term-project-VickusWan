0024c3d33187df1d9b5cf853416724bc6c520934
VickusWan <vuong@mail.sfsu.edu>
2023-10-23 10:19:30 -0700

    first run

---
+++ backend/routes/root.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", (request, response) => {
+    response.send("Hello world from within a route!");
+    })
+
+module.exports = router;
