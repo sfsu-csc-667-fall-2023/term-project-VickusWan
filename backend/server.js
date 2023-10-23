6ad23747e9662f091fbc833b0f96cb7c5342001c
VickusWan <vuong@mail.sfsu.edu>
2023-10-23 10:55:32 -0700

    webpack updates

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,31 @@ const path = require("path");
 const express = require("express");
 const createError = require("http-errors");
 const requestTime = require("./middleware/request-time");
-
+const rootRoutes = require("./routes/root");
 
 const app = express();
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "static")));
 
 app.use(requestTime);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "static")));
+if (process.env.NODE_ENV === "development") {
+    const livereload = require("livereload");
+    const connectLiveReload = require("connect-livereload");
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+    liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+    liveReloadServer.refresh("/");
+    }, 100);
+    });
+    app.use(connectLiveReload());
+}
 
-const rootRoutes = require("./routes/root");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "static")));
 
 app.use("/", rootRoutes);
 
